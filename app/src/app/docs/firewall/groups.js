223792de042259b59d41ecb002de365f33152dff
Dorian Maliszewski <maliszewskid3@gmail.com>
2024-07-24 19:27:08 +0200

    fix: some texts

--- app/src/app/docs/firewall/groups.js
+++ app/src/app/docs/firewall/groups.js
@@ -2,7 +2,7 @@ export default function Groups() {
   return (
     <section id="groups">
       <h2>
-        Configuration des Network Groups et Address Groups
+        Network Groups et Address Groups
       </h2>
       <p>
         Ce guide explique comment configurer les Network Groups et Address
@@ -11,13 +11,7 @@ export default function Groups() {
       </p>
 
       <h3>Configuration via la CLI</h3>
-      <h4>Étape 1 : Accéder à la CLI</h4>
-      <p>Connectez-vous au Edge Router via SSH.</p>
-      <ul>
-        <code>ssh username@192.168.1.1</code>
-      </ul>
-
-      <h4>Étape 2 : Créer un Network Group</h4>
+      <h4>Étape 1 : Créer un Network Group</h4>
       <p>Entrez en mode configuration et ajoutez un Network Group.</p>
       <ul>
         <li>
@@ -28,15 +22,9 @@ export default function Groups() {
             set firewall group network-group LAN-NETWORK network 192.168.1.0/24
           </code>
         </li>
-        <li>
-          <code>commit</code>
-        </li>
-        <li>
-          <code>save</code>
-        </li>
       </ul>
 
-      <h4>Étape 3 : Créer un Address Group</h4>
+      <h4>Étape 2 : Créer un Address Group</h4>
       <p>En mode configuration, ajoutez un Address Group.</p>
       <ul>
         <li>
@@ -48,15 +36,6 @@ export default function Groups() {
           <code>
             set firewall group address-group ALLOWED-HOSTS address 192.168.1.20
           </code>
-        </li>
-        <li>
-          <code>commit</code>
-        </li>
-        <li>
-          <code>save</code>
-        </li>
-        <li>
-          <code>exit</code>
         </li>
       </ul>
 
